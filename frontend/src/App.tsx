a5b0dab52314a2ba2741616ff0f173ce29ca7c61
Sunny Kumar <goodluck14115@gmail.com>
2025-04-07 08:33:44 +0530

    feat: install vite and setup prettier & tailwindcss in frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex items-center justify-center h-screen w-full'>
+      <h1 className='text-5xl font-semibold text-center text-blue-600'>IntelliSQR Assignment</h1>
+    </div>
+  )
+}
+
+export default App
